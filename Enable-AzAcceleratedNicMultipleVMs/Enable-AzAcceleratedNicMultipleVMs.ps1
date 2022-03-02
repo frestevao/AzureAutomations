85f54ac34c9aae01c871d1dc94d5fc5023b7868d
Estevão França <31082826+frestevao@users.noreply.github.com>
2022-03-02 14:06:45 -0300

    Update Enable-AzAcceleratedNicMultipleVMs.ps1

--- Enable-AzAcceleratedNicMultipleVMs/Enable-AzAcceleratedNicMultipleVMs.ps1
+++ Enable-AzAcceleratedNicMultipleVMs/Enable-AzAcceleratedNicMultipleVMs.ps1
@@ -9,8 +9,6 @@
     You need to inform the tenantId, in case your user has access to more than one Tenant.
 .PARAMETER ResourceGroupName    
     Resource Group where you store your VM's
-.DESCRIPTION
-    This script will enable the Accelerated Nerworking in multiple VM's under the same RG
 .Example 
     .\Enable-AzAcceleratedNicMultipleVMs.ps1 -subscriptionId <SubscriptionId> -tenantId <TenantId> -resourceGroupName <ResourceGroupName>
 .NOTES
@@ -44,6 +42,7 @@ if($null -eq $subscriptionId -or $null -eq $tenantId -or $null -eq $resourceGrou
 $enableAcceleratedNic = $True #The Value Should be True or False
 #Getting list of VM's under the RG
 $azureVM = az vm list -g $resourceGroupName | ConvertFrom-Json
+
 foreach($vm in $azureVM){
     #Getting VM details
     $vmSetting = az vm show -n $vm.name -g $resourceGroupName | ConvertFrom-Json
@@ -57,15 +56,18 @@ foreach($vm in $azureVM){
     [string]$vmSize = "'" + $vmSetting.hardwareProfile.vmSize + "'"
     $vmNumberOfCores = az vm list-sizes -l $vmSetting.location --query "[?contains(name, $vmSize)]" | ConvertFrom-Json
     #Checking if VM is part of an availability set
+    
     if($null -eq $vmSetting.availabilitySet.id){
         Write-Host " This vm is not part of an availability set"
         Write-Host "Moving forward with the deployment..."
     }else{
         Write-Warning -Message "This VM is part of an availability set and due this, the deployment may fail. If you get an error, please check the Microsoft Documentation" 
     }
+    
     Write-Host ""
     Write-Host ""
     # This condition checks if the Accelerated network is already enabled, if it is, the script will ignore the VM and got to the next
+    
     if($nicState.enableAcceleratedNetworking -eq $false -and $vmNumberOfCores.NumberOfCores -ge 4){
         Write-Host "Accelerated networking is not enabled..."
         Write-Host "Enabling..."
@@ -75,23 +77,27 @@ foreach($vm in $azureVM){
         $status = az vm get-instance-view --name $vm.name --resource-group $resourceGroupName --query instanceView.statuses[1] -o json | ConvertFrom-Json
         Start-Sleep -Seconds 5
         Write-Host ""
+        
         if($status.displayStatus -eq "VM deallocated"){
             Write-Host "VM already deallocated" -ForegroundColor Green
         }else{
             Write-Host "The VM" $vm.name "is running and will be deallocated" -ForegroundColor Red
             az vm deallocate --name $vm.name --resource-group $resourceGroupName
         }
+        
         Write-Host ""
         #enabling ...
         Write-Host ""
         Write-Host "Enabling Accelerated Networking..."
         $enableAccelerated = az network nic update --name $vmSetting.networkProfile.networkInterfaces.Id.Split('/')[8] --resource-group $resourceGroupName --accelerated-networking $enableAcceleratedNic
         $nicState2 = az network nic show --name $vmSetting.networkProfile.networkInterfaces.Id.Split('/')[8] --resource-group $resourceGroupName | ConvertFrom-Json
+        
         if($nicState2.enableAcceleratedNetworking -ne $true) {
             Write-Host "Error -> Accelerated Network was not enabled on" $vm.name -ForegroundColor Red
         }else{
             Write-Host "Success -> Accelerated Network was enabled on"  $vm.name -ForegroundColor Green
         }
+        
         #Starting VM
         Write-Host ""
         Write-Output "Starting VM..."
