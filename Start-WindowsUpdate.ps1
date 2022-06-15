b88399cc5f12e40c95e46a7a11eccfa49841be92
Estevão França <31082826+frestevao@users.noreply.github.com>
2022-06-15 17:24:55 -0300

    Create Start-WindowsUpdate.ps1

---
+++ Start-WindowsUpdate.ps1
@@ -0,0 +1,66 @@
+ #Get-Command -Module *WindowsUpdateProvider*
+#Get-Content Function:\Start-WUScan
+#-SearchCriteria "Type='Software' AND IsInstalled=0"
+
+#Checking if the Log path Exist
+
+$logPath = Test-Path -Path "C:\scriptlogs"
+
+if ($logPath -eq $false){
+
+    Write-Error -Message "Log path doesn't exist"
+}else {
+    Write-Output "Log path does exist"
+}
+
+$updates = Start-WUScan 
+Write-Output "======================================================" >> C:\scriptlogs\log.txt
+Write-Output "List of updates that will be applied" >> C:\scriptlogs\log.txt
+Write-Output "======================================================" >> C:\scriptlogs\log.txt
+Write-Output ""  >> C:\scriptlogs\log.txt
+
+foreach ($update in $updates){
+
+Write-Output $update.Title >> C:\scriptlogs\log.txt
+
+}
+
+Write-Output ""  >> C:\scriptlogs\log.txt
+Write-Output "======================================================" >> C:\scriptlogs\log.txt
+Write-Output "End of the list of updates" >> C:\scriptlogs\log.txt
+Write-Output "======================================================" >> C:\scriptlogs\log.txt
+
+Write-Output ""  >> C:\Users\admstvao\Desktop\logs\log.txt
+Write-Output "======================================================" >> C:\scriptlogs\log.txt
+Write-Output "Starting the patching application" >> C:\scriptlogs\log.txt
+Write-Output "======================================================" >> C:\scriptlogs\log.txt
+
+$installUpdates = Install-WuUpdates (Start-WUScan)
+
+if ($installUpdates -eq $true){
+
+    Write-Output "Patches were applied with success" >> C:\scriptlogs\log.txt
+
+    $rebootstatus = Get-WUIspendingReboot
+
+   if ($rebootstatus -eq $true){
+    
+        Write-Output "Patches applied and the server will be rebooted"  >> C:\scriptlogs\log.txt
+
+        Restart-Computer -Force
+   
+   }else{
+   
+        Write-Output "Unknown error"  >> C:\scriptlogs\log.txt
+    
+    }
+
+}Else{
+
+    Write-Output "Unknown error " >> C:\scriptlogs\log.txt
+}
+
+Write-Output ""  >> C:\scriptlogs\log.txt
+Write-Output "======================================================" >> C:\scriptlogs\log.txt
+Write-Output "End of the code" >> C:\scriptlogs\log.txt
+Write-Output "======================================================" >> C:\scriptlogs\log.txt 
